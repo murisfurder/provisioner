4f40c71ed545652b61461df8834838958a206d62
Viktor Petersson <vpetersson@wireload.net>
2016-05-05 15:44:25 +0100

    Rewrites docker example.

--- example/python_docker_in_vagrant.py
+++ example/python_docker_in_vagrant.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python
 
-import python_example
+import settings
+from python_example import get_status, create_task
 from time import sleep
 
-job_id = python_example.create_task(
-    ip='192.168.33.10',
-    username='vagrant',
-    password='foobar123',
-    role='docker'
-)
 
-finished = False
-while not finished:
-    exit_status = ['Aborted', 'Done', 'Error']
-    status = python_example.get_status(job_id)
-    if status['status'] in exit_status:
-        DONE = True
-        break
+def create_docker_nodes(max_retries=100):
+    tasks = []
+
+    for node in settings.NODES:
+        tasks.append(create_task(
+            ip=node['ip'],
+            username=node['username'],
+            password=node['password'],
+            role='docker',
+        ))
+
+    while max_retries > 0:
+        max_retries -= 1
+
+        if len(tasks) < 1:
+            return True
+
+        for task in tasks:
+            status = get_status(task)
+            print 'Task docker ({}) status is {}'.format(
+                task,
+                status['status']
+            )
+            if status['status'] in settings.EXIT_STATUS:
+                print 'Task weave ({}) exited.'.format(task)
+                tasks.remove(task)
+        sleep(5)
     else:
-        print status
-    sleep(5)
+        return False
+
+
+def main():
+    create_docker_nodes()
+
+if __name__ == "__main__":
+    main()
