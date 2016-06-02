a9512ff1a2a902dcabbbe21f678636f408e7f9a6
Viktor Petersson <vpetersson@wireload.net>
2016-06-02 18:39:18 +0100

    Adds test for doc parsing.

---
+++ tests/test_doc_parser.py
@@ -0,0 +1,24 @@
+from nose.tools import eq_, assert_in
+from lib import doc_parser
+
+
+def test_non_existing_docs():
+    r = doc_parser.get_docs(role='foobar')
+    eq_(r, 'No installation notes available.')
+
+
+def test_drupal_without_vars():
+    r = doc_parser.get_docs(role='drupal')
+    eq_(r, 'Failed to process installation notes.')
+
+
+def test_drupal_with_vars():
+    r = doc_parser.get_docs(
+        role='drupal',
+        extra_vars={
+            'public_ip': '127.0.0.1',
+            'postgres_drupal_password': 'foobar',
+            'postgres_postgres_password': 'foobar',
+        }
+    )
+    assert_in('Congratulations!', r)
