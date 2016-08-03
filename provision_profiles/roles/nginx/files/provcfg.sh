0b6e3477598a52716cb92ee74021454590fa9921
Viktor Petersson <vpetersson@wireload.net>
2016-08-03 18:58:00 +0100

    Simplifies Let’s Encrypt and adds documentation.

--- provision_profiles/roles/nginx/files/provcfg.sh
+++ provision_profiles/roles/nginx/files/provcfg.sh
@@ -3,6 +3,28 @@
 NGINXCONF=/etc/nginx/conf.d/default.inc
 SSLCONF=/etc/nginx/conf.d/ssl-redirect.inc
 
+SUBDOMAINS=(
+docker-registry
+drupal joomla
+nodebb
+owncloud
+redmine
+wordpress
+)
+
+function generate_cert {
+  FQDN="$1"
+  EMAIL="$2"
+  PARMS=(certonly -n --expand --agree-tos --email $2 --webroot -w /var/www)
+  PARMS+=(-d $FQDN)
+
+  for i in ${SUBDOMAINS[*]}; do
+    PARMS+=(-d $i.$FQDN)
+  done
+
+  /usr/local/sbin/certbot-auto "${PARMS[@]}"
+}
+
 function default_stanza {
   echo "location / {"
   echo "    root /var/www;"
@@ -23,6 +45,8 @@ function print_help {
   echo '$ provcfg set-default <app>'
   echo 'To revert to the default (no app):'
   echo '$ provcfg set-default none'
+  echo 'To generate a Lets Encrypt certificate, run:'
+  echo '$ provcfg generate-cert mydomain.com john@doe.com'
   exit 1
 }
 
@@ -42,6 +66,14 @@ if [ "$1" == "set-default" ]; then
     echo "Setting $2 as the default webapp."
     set_app_as_default "$2" > $NGINXCONF
   fi
+elif [ "$1" == "generate-cert" ]; then
+  if [ -z "$2" ]; then
+    print_help
+  elif [ -z "$2" ]; then
+    print_help
+  fi
+  echo "Generating SSL certificate for $1."
+  generate_cert "$2" "$3"
 else
   print_help
 fi
